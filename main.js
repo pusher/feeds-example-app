54fbac949b238f6daef76cf7499ea440072931bf
Will Sewell <me@willsewell.com>
2017-06-01 12:01:44 +0100

    more stuff

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-sesison");
+const bodyParser = require("body-parser");
 const Feeds = require("./pusher-feeds-server");
 
 const feeds = new Feeds({
@@ -7,20 +8,45 @@ const feeds = new Feeds({
   appKey: "the-id-bit:the-secret-bit"
 });
 
-function hasPermission(userId, feedId, type) {
-  return true;
+function hasPermission(userId, feedId) {
+  if (userId === "admin") {
+    return true;
+  }
+  return `private-${userId}` === feedId);
 }
 
 const app = express();
 app.use(session({ secret: "blah" }));
+app.use(bodyParser.json())
 
-app.get("/notes", (req, res) => {
-  req.session.userId = req.query.user_id;
-})
+app.get("/", (req, res) => {
+  res.sendFile("login.html");
+});
+
+app.get("/notes/:user_id", (req, res) => {
+  req.session.userId = req.params.user_id;
+  res.sendFile("notes.html");
+});
+
+app.post("/newsfeed", (req, res) => {
+  feeds.publish("newsfeed", req.body);
+});
+
+app.post("/notes/:user_id", (req, res) => {
+  const feedId = `private-${req.params.user_id}`
+  if (hasPermission(req.session.userId, feedId)) {
+    feeds.publish(feedId, req.body);
+  } else {
+    res.sendStatus(401)
+  }
+});
 
 app.get("/feeds/tokens", (req, res) => {
   feeds.authorize(req, res, (feedId, type) => {
-    return hasPermission(session.userId, feedId, type);
+    if (type !== 'WRITE') {
+      return false;
+    }
+    return hasPermission(session.userId, feedId);
   });
 });
 
