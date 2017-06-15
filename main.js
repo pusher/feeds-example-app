64d5ff8b90a39f412269c5383cc2d6254a2b8eaf
Karel Moravec <karel@pusher.com>
2017-06-15 18:18:25 +0100

    Update example with new SDKs versions

--- main.js
+++ main.js
@@ -1,59 +1,91 @@
-const fs = require("fs");
-const express = require("express");
-const session = require("express-session");
-const bodyParser = require("body-parser");
+import { readFile } from 'fs';
+import {join as joinPath} from 'path';
 
-const Feeds = require("./pusher-feeds-server");
+import express from 'express';
+import session from 'express-session';
+import bodyParser from 'body-parser';
 
-const feeds = new Feeds({
-  appId: "auth-example-app",
-  appKey: "the-id-bit:the-secret-bit",
-  host: "api-staging-ceres.kube.pusherplatform.io"
+import Service from 'pusher-feeds-server';
+
+const feeds = new Service({
+  serviceId: 'auth-example-app',
+  serviceKey: 'the-id-bit:the-secret-bit',
+  host: 'api-staging-ceres.kube.pusherplatform.io'
 });
 
 function hasPermission(userId, feedId) {
-  return userId === "big-brother" || feedId === `private-${userId}`;
+  return userId === 'big-brother' || feedId === `private-${userId}`;
 }
 
 const app = express();
-app.use(express.static("static"));
-app.use(session({ secret: "HvCYzkbSjv3hNUf3fetPChO7DNxNPuOB" }));
-app.use(bodyParser.json());
 
-app.get("/login", (req, res) => {
+app.use(express.static(joinPath(process.cwd(), 'static')));
+app.use(session({ secret: 'HvCYzkbSjv3hNUf3fetPChO7DNxNPuOB' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Auth user and redirect to main page
+app.get('/', (req, res) => {
+  readFile(joinPath(process.cwd(), 'index.html'), 'utf8', (err, data) => {
+    res.type('html');
+    res.send(data);
+  });
+});
+
+app.get('/login', (req, res) => {
   req.session.userId = req.query.user_id;
   res.redirect(`/notes/${req.query.user_id}`);
 });
 
-app.get("/notes/:note_id", (req, res) => {
-  // Hacky templating to embed the user ID
-  fs.readFile("notes.html.template", "utf8", (err, data) => {
-    html = data.replace(/\$NOTE_ID/g, req.params.note_id)
-      .replace(/\$USER_ID/g, req.session.userId);
-    res.type("html");
-    res.send(html);
-  });
-});
-
-app.post("/newsfeed", (req, res) => {
-  feeds.publish("newsfeed", [ req.body ]);
-  res.sendStatus(204);
-});
-
-app.post("/notes/:user_id", (req, res) => {
-  const feedId = `private-${req.params.user_id}`
-  if (hasPermission(req.session.userId, feedId)) {
-    feeds.publish(feedId, [ req.body ]);
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(401);
+// Render template with public and private feeds for logged user
+app.get('/notes/:note_id', (req, res) => {
+  if (!req.session.userId) {
+    res.redirect('/');
+    return;
   }
+
+  // Hacky templating to embed the user ID
+  readFile(joinPath(process.cwd(), 'notes-template.html'), 'utf8', (err, data) => {
+    res.type('html');
+    res.send(
+      data
+        .replace(/\$NOTE_ID/g, req.params.note_id)
+        .replace(/\$USER_ID/g, req.session.userId)
+    );
+  });
 });
 
-app.get("/feeds/tokens", (req, res) => {
-  feeds.authorize(req, res, {}, (feedId, type) => {
-    return type === "READ" && hasPermission(req.session.userId, feedId);
-  });
+// Publish data into private feed
+app.post('/notes/:user_id', (req, res) => {
+  const feedId = `private-${req.params.user_id}`
+
+  if (!hasPermission(req.session.userId, feedId)) {
+    return res.sendStatus(401);
+  }
+
+  feeds
+    .publish(feedId, req.body)
+    .then(() => res.sendStatus(204))
+    .catch(err => res.status(400).send(err));
+});
+
+// Publis data into public feed
+// Does not require any authe
+app.post('/newsfeed', (req, res) => {
+  feeds
+    .publish('newsfeed', req.body)
+    .then(data => res.sendStatus(204))
+    .catch(err => res.status(400).send(err));
+});
+
+app.post('/feeds/tokens', (req, res) => {
+  const validateRequest = (action, feedId) => action === 'READ' && hasPermission(req.session.userId, feedId);
+
+  feeds.authorizeFeed(req, validateRequest)
+    .then(data => res.send(data))
+    .catch(err => {
+      res.status(400).send(`${err.name}: ${err.message}`) 
+    });
 });
 
 const port = process.env.PORT || 5000;
---
+++ readme.md
@@ -0,0 +1,3 @@
+Example of usage https://github.com/pusher/feeds-client-js and https://github.com/pusher/feeds-server-node toghether.
+
+Currently just fetching packages from github and build them in postinstall script.
